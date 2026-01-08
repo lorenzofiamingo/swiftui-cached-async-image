9fb30b8fb610794b18f3886375e1683a3b0598dd
Wes Mills <34358966+wesmi@users.noreply.github.com>
2026-01-08 05:49:05 -0800

    Add comma to instance
    Comma at end of preceding value is needed to add visionOS to the list of values instead of as a static member

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
-        .watchOS(.v6)
+        .watchOS(.v6),
         .visionOS(.v1)
     ],
     products: [
